f423e7690bbce958c60b4d646d565362951140a3
Daniel77640 <matos29@gmail.com>
2025-01-30 22:45:38 +0000

    final

---
+++ Teste 28-01/30-01-2025.py
@@ -0,0 +1,17 @@
+nome="Daniel Ruivo Matos"
+proprio="Daniel"
+apelido="Matos"
+y=2
+PY=3,14
+aux="mapa"
+elevado=4**2
+
+a=3
+b=5
+
+numero = input("intruduz um numero inteiro")
+numero= int(numero)
+
+dobro=2*numero
+
+print("O 5dobro de ", numero, "e", dobro )
