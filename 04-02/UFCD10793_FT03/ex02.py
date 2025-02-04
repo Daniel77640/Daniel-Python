d8f2e87bd97659f9e7af6d51d119a2e025ebabc8
Daniel77640 <matos29@gmail.com>
2025-02-04 22:46:57 +0000

    04-02

---
+++ 04-02/UFCD10793_FT03/ex02.py
@@ -0,0 +1,21 @@
+"""Implementa um programa que leia dois números e indique se estes são iguais ou
+diferentes."""
+
+numero1 = float(input("numero:"))
+numero2 = float(input("numero:"))
+
+if numero1 == numero2:
+    print("os numeros são iguais")
+else:
+    print("os numeros são diferentes")
+
+
+#resolução
+
+
+numero1 = float(input("Número:"))
+numero2 = float(input("Número:"))
+if numero1 == numero2:
+    print("Os números são iguais")
+else:
+    print("Os números são diferentes")
