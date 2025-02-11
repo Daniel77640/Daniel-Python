5dddc2c0f81fe8c9b63658b630ed7924108c811a
Daniel77640 <matos29@gmail.com>
2025-02-11 21:56:36 +0000

    11-2

---
+++ 11-02/ficha4 ex8.py
@@ -0,0 +1,6 @@
+"""Escreve um programa que coloque no ecrã a tabuada do número 5"""
+
+for i in range (1, 11, 1):
+    j = 5
+    multiplicação= i * j
+    print (f"{j} * {i} = {multiplicação}")
---
+++ 11-02/ficha4 ex9.py
@@ -0,0 +1,3 @@
+"""Reescreve o programa anterior de forma a apresentar a tabuada de qualquer número
+introduzido pelo utilizador."""
+
