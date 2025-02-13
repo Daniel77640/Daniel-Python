d8cb4f02c3404d546b2d7250c9678509f3c1ac63
Daniel77640 <matos29@gmail.com>
2025-02-13 22:25:15 +0000

    13-2

---
+++ 13-12/ficha6ex3.py
@@ -0,0 +1,19 @@
+"""Considera a lista
+idades=[25, 15, 19, 22, 37, 78, 46, 2, 67]
+Cria um programa, em python, que:
+a. Indique quantas pessoas são menores de idade
+b. Ordene a lista por ordem decrescente
+c. Pede ao utilizador uma idade e verifica se essa idade está na lista.
+#- Se estiver faz print("A idade está na lista")
+#- Caso contrário faz o print("não existe ninguém com essa idade na lista")"""
+
+idades=[25, 15, 19, 22, 37, 78, 46, 2, 67]
+print(idades (<18))
+idades.sort(reverse = True)
+x1= input ("indique uma idade")
+if x1 in idades
+print ("A idade está na lista")
+else
+print("não existe ninguém com essa idade na lista")
+
+
