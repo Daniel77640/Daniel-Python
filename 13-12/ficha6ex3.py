5d854e02545059b6a0059bc18fbec40b13a453e7
Daniel77640 <matos29@gmail.com>
2025-02-13 22:50:32 +0000

    13-2

--- 13-12/ficha6ex3.py
+++ 13-12/ficha6ex3.py
@@ -8,12 +8,22 @@ c. Pede ao utilizador uma idade e verifica se essa idade está na lista.
 #- Caso contrário faz o print("não existe ninguém com essa idade na lista")"""
 
 idades=[25, 15, 19, 22, 37, 78, 46, 2, 67]
-print(idades (<18))
+
+x=0
+for idade in idades:
+    if idade <18:
+        x+=1
+print(x)
+
+    
 idades.sort(reverse = True)
-x1= input ("indique uma idade")
-if x1 in idades
-print ("A idade está na lista")
-else
-print("não existe ninguém com essa idade na lista")
+print (idades)
+
+
+x1= input ("indique uma idade ")
+if x1 in idades:
+    print ("A idade está na lista")
+else:
+    print("não existe ninguém com essa idade na lista")
 
 
