0777f94c1e547ebe76ebf96c85439f86ef84ce05
Daniel77640 <matos29@gmail.com>
2025-02-13 21:20:17 +0000

    13-02

---
+++ 13-12/1 - listas.py
@@ -0,0 +1,10 @@
+nomelista = ["preto", "branco", "cinza", "amarelo", "verde", "azul", "vermelho"]
+outralista = list(("verde", "azul", "vermelho"))
+outralista1 = list[0, 1]
+print(nomelista)
+print (outralista)
+print (nomelista[3:5])
+print (nomelista[3:])
+print (nomelista[:3])
+print (nomelista[-1])
+print (nomelista[-4])
---
+++ 13-12/ficha4 ex17.py
@@ -0,0 +1,8 @@
+"""Escreva um programa que peça ao utilizador 20 números reais e no final mostre a soma e a média
+dos números introduzidos."""
+
+x1 = 1
+X2= x1 +1
+
+Input (float("intruduza {x1} de 20 numeros inteiros  "))
+Input (float("intruduza {x2} de 20 numeros inteiros  "))
