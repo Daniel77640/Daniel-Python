53ce6d893115fb3ecf752ba9d87185dee0c7fa58
Daniel77640 <matos29@gmail.com>
2025-02-27 21:25:11 +0000

    25-02

--- Avaliação intermedia/banco_dados.py
+++ Avaliação intermedia/banco_dados.py
@@ -1,19 +1,16 @@
 import sqlite3
 
-conexao = sqlite3.connect("universidade.db")
-
-cursor = conexao.cursor()
+conn = sqlite3.connect ("empresa.db")
+cursor = conn.cursor()
 
 cursor.execute ("""
 CREATE TABLE IF NOT EXISTS Alunos (
     id INTEGER PRMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     idade INTEGER,
-    curso TEXT)
+    curso TEXT
+    )
 """)
 
-
-print ("tabela criada com sucesso!")
-
-conexao.commit()
-conexao.close()
+conn.commit()
+conn.close()
