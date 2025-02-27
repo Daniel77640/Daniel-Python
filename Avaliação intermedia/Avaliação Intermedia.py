b4f3bd2dc76c01c882cc1f64ce9134de29224689
Daniel77640 <matos29@gmail.com>
2025-02-27 22:40:45 +0000

    27-2

--- Avaliação intermedia/Avaliação Intermedia.py
+++ Avaliação intermedia/Avaliação Intermedia.py
@@ -2,6 +2,23 @@ import sqlite3
 conn = sqlite3.connect("empresa.db")
 cursor = conn.cursor()
 
-cursor.execute("INSERT INTO funcionarios (nome, cardgo, salario) VALUES ("Ana Silva", "Gestora", "3500")")
-cursor.execute("INSERT INTO funcionarios (nome, cardgo, salario) VALUES ("Pedro Santos", "Programador", "1500")")
-cursor.execute("INSERT INTO funcionarios (nome, cardgo, salario) VALUES ("MAriana Costa", "Designer", "3000")")
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Ana Silva', 'Gestora', 3500)")
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Pedro Santos', 'Programador', 1500)")
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Mariana Costa', 'Designer', 3000)")
+
+conn.commit()
+conn.close()
+
+#Acrescentar mais 2 funcionarios
+
+import sqlite3
+conn = sqlite3.connect("empresa.db")
+cursor = conn.cursor()
+
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Daniel Matos', 'SubBigBoss', 6000)")
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Pedro PEreira', 'BigBoss', 2500)")
+
+conn.commit()
+conn.close()
+
+cursor.execute("empresa.db")
